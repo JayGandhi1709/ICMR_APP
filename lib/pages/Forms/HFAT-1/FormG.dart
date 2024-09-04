6dfbf6b205ee6d27bd6471da9781ca3cd8ca34fd
Jash Patel <www.jashpatel211@gmail.com>
2024-09-04 12:27:19 +0530

    Folder Struct. change

---
+++ lib/pages/Forms/HFAT-1/FormG.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class FormG extends StatefulWidget {
+  const FormG({super.key});
+
+  @override
+  State<FormG> createState() => _FormGState();
+}
+
+class _FormGState extends State<FormG> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

57c74c144da7707a4479ad74f269fb85cfdd94e4
Your Name <you@example.com>
2024-09-11 20:45:31 +0530

    All tabs done!

--- lib/pages/main_home_screen.dart
+++ lib/pages/main_home_screen.dart
@@ -3,11 +3,13 @@ import 'package:icmr/pages/HomePage/home_page.dart';
 import 'package:icmr/pages/HomePage/signIn_homePage.dart';
 import 'package:icmr/pages/ProjectSite/projectsite.dart';
 import 'package:icmr/pages/SignInPage/signin.dart';
+import 'package:icmr/pages/Objective/objective.dart';
+import 'package:icmr/pages/Outcomes/outcomes.dart';
 import 'package:icmr/utils/custom_app_bar.dart';
 import 'package:icmr/utils/custom_nav_bar.dart';
-import 'package:icmr/utils/signIn_navbar.dart';
 
 import 'AboutProject/about_project.dart';
+import 'Methodology/methodology.dart';
 
 class MainHomePage extends StatefulWidget {
   const MainHomePage({super.key});
@@ -17,15 +19,17 @@ class MainHomePage extends StatefulWidget {
 }
 
 class _MainHomePageState extends State<MainHomePage> {
-  bool isLogin = false;
+  bool isLogin = true;
   int selectedIndex = 0;
   List<Widget> loginScreens = [
     const SigninHomepage(),
-    const AboutProjectPage(),
-    const ProjectSites(),
-    const Text("research Team"),
-    const Text("what's new"),
-    const SignIn(),
+    const Objective(),
+    const OutcomePage(),
+    const Methodology(),
+    const Text("Facilities"),
+    const Text("Workflow"),
+    const Text("Data Collection"),
+    const HomePage(),
   ];
   List<Widget> afterLoginScreens = [
     const HomePage(),
@@ -65,15 +69,6 @@ class _MainHomePageState extends State<MainHomePage> {
         padding: const EdgeInsets.all(16),
         child: Column(
           children: [
-            // signInNavBar(
-            //   selectedIndex: selectedIndex,
-            //   onTap: (int index) {
-            //     setState(() {
-            //       selectedIndex = index;
-            //     });
-            //   },
-            // ),
-
             customNavBar(
               selectedIndex: selectedIndex,
               onTap: (int index) {
@@ -83,7 +78,6 @@ class _MainHomePageState extends State<MainHomePage> {
               },
               navItem: isLogin ? loggedInNavItem : navItem,
             ),
-
             Expanded(child: tabs[selectedIndex]),
           ],
         ),

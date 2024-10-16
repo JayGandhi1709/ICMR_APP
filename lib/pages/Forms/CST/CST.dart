241ef0d2ba3d561f6dd76701b001fdae7f19ee7b
Your Name <you@example.com>
2024-10-16 14:32:46 +0530

    CST Folder

---
+++ lib/pages/Forms/CST/CST.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:icmr/components/Layout/CST_Layout/CST_Forms_Layout.dart';
+import 'package:icmr/pages/Forms/HFAT-1/FormD.dart';
+
+import '../../../components/Layout/HFAT_Layout/forms_layout.dart';
+import 'CST_Form-A.dart';
+
+class CST extends StatefulWidget {
+  const CST({super.key});
+
+  @override
+  State<CST> createState() => _CSTState();
+}
+
+class _CSTState extends State<CST> {
+  bool isDataSubmited = false;
+  int currentFormIndex = 0;
+  final _formKey = GlobalKey<FormState>();
+
+  final sidepanelList = [
+    "Site Information",
+    "1B. Infrastructure",
+    "1C. Human Resources",
+    "1D. Logistics (Drugs/ Consumables/ Equipment)",
+    "1E. Emergency Care Services",
+    "1F. Information System",
+    "1G. Finance",
+    "1H. Leadership and Governance",
+    "1I. Process/ Policies/SOPs",
+    "1J. Referral Linkages",
+  ];
+
+  final List listOfQuestion = [cstFormA];
+
+  @override
+  Widget build(BuildContext context) {
+    return CstFormsLayout(
+      currentSideIndex: currentFormIndex,
+      sidePanelList: sidepanelList,
+      formKey: _formKey,
+      questions: listOfQuestion[currentFormIndex],
+      heading: "Community Survey Tool",
+      title: "A Socio-demographic Characteristics",
+      subtitle: sidepanelList[currentFormIndex],
+      prevText: currentFormIndex == 0 ? '' : "Previous",
+      onPrevious: () {
+        setState(() {
+          if (currentFormIndex > 0) {
+            currentFormIndex -= 1;
+          }
+        });
+      },
+      onNext: () {
+        setState(() {
+          if (currentFormIndex < listOfQuestion.length - 1) {
+            currentFormIndex += 1;
+          }
+        });
+      },
+    );
+  }
+}
